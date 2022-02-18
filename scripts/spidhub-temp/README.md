e09a87396d69fc7a31bc1d75009aa3b93e96778b
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-02-18 17:59:25 +0100

    Feature/pn 669 (#19)
    * PN-669 script per creazione immagine EC2 con spidhub
    
    
    Co-authored-by: marco-vit-pagopa <marco.vit@pagopa.it>

---
+++ scripts/spidhub-temp/README.md
@@ -0,0 +1,22 @@
+# SpidHub for test environments
+
+Scripts to deploy a temporary solution to use the SPID login for dev and test environments.
+
+This folder contains scripts to deploy and configure an EC2 instance with this containers:
+
+- [spid-testenv2](https://github.com/italia/spid-testenv2): SPID Idp and Sp for test purpose
+- [hub-spid-login-ms](https://github.com/pagopa/hub-spid-login-ms)
+  - Entry point for SPID login 
+  - Integration with SelfCare UserRegistry
+  - JWT integration
+
+The _create-new-spidhub.sh_ script automate the EC2 instance creation using the _cfn-templates/vpc_with_public_ec2.yaml_
+Cloud Formation template, creating the key pair to connect via ssh and exposing it on the DNS domain.
+
+It store the private key in _$HOME/.spidhub_keys_ directory.
+
+All file in _remote-scripts_ directory are copied in the EC2 instance and used to:
+1. install _docker_ and _docker compose_
+2. clone the [hub-spid-login-ms](https://github.com/pagopa/hub-spid-login-ms) repo
+3. apply the needed configuration (create certificate for SAM, key pairs for JWT, configure _.env_ file)
+4. launch container with docker compose
---
+++ scripts/spidhub-temp/remote-scripts/install.sh
@@ -0,0 +1,104 @@
+echo -e "\n\n\n"
+echo "===              INSTALL REQUIRED SOFTWARE PACKAGES              ==="
+echo "===================================================================="
+echo ""
+echo "=== Update package repository"
+
+sudo yum update -y
+
+echo ""
+echo "=== Install git from yum"
+sudo yum install -y git
+
+echo ""
+echo "=== Install java from amazon-linux-extras"
+sudo amazon-linux-extras install -y java-openjdk11
+
+echo ""
+echo "=== Install docker from amazon-linux-extras"
+sudo amazon-linux-extras install docker
+
+echo ""
+echo "=== Install docker from yum"
+sudo yum install -y docker
+
+echo ""
+echo "=== Activate docker service"
+echo "= start"
+sudo service docker start
+echo "= enable"
+sudo systemctl enable docker
+echo "= usermod"
+sudo usermod -a -G docker ec2-user
+echo "= info"
+sudo docker info
+
+
+echo "= download plugin"
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+echo ""
+echo "=== Install node and yarn"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install yarn -g
+
+
+
+echo -e "\n\n\n"
+echo "===           DOWNLOAD AND CONFIGURE SPIDHUB REPOSITORY           ==="
+echo "======================================================================"
+echo ""
+echo "=== Clone repository"
+git clone https://github.com/pagopa/hub-spid-login-ms.git
+
+echo "= change dir"
+cd hub-spid-login-ms
+echo "= NEW CURRENT WORKING DIRECTORY: $(pwd)"
+
+echo ""
+echo "=== Create SAML certificates"
+./scripts/make-certs.sh
+
+echo ""
+echo "=== Create JWT key pair"
+./scripts/generate-rsa-jwt-key-pair.sh 
+
+echo ""
+echo "=== Build well-known JWKS json"
+mkdir -p "$HOME/hub-spid-login-ms/well-known/"
+( cd $HOME/build-jwks && ./mvnw clean compile exec:java -Dexec.mainClass="buildjwks.BuildJwks"  \
+    -Dexec.args="$HOME/hub-spid-login-ms/jwt_rsa_public.pem $HOME/hub-spid-login-ms/well-known/jwks.json" )
+
+echo ""
+echo "=== Prepare parameters"
+
+DnsFullDomain=$(cat ../full-dns-name | tr -d '\n')
+echo "= DnsFullDomain: ${DnsFullDomain}"
+
+MakecertPrivate=$( cat certs/key.pem | sed -e 's/$/\\n/' | tr -d '\n' | sed -e 's/\\n$//')
+echo "= MakecertPrivate: ${MakecertPrivate}"
+
+MakecertPublic=$( cat certs/cert.pem | sed -e 's/$/\\n/' | tr -d '\n' | sed -e 's/\\n$//' )
+echo "= MakecertPublic: ${MakecertPublic}"
+
+LoginSuccessDestinationEnpoint=$( cat ../login-success-destination-enpoint | tr -d '\n' )
+echo "= LoginSuccessDestinationEnpoint: ${LoginSuccessDestinationEnpoint}"
+
+JwtTokenPrivateKey=$( cat jwt_rsa_key.pem | sed -e 's/$/\\n/' | tr -d '\n' | sed -e 's/\\n$//' )
+echo "= JwtTokenPrivateKey: ${JwtTokenPrivateKey}"
+
+echo ""
+echo "=== Generate .env and conf-testenv/config.yaml"
+export DnsFullDomain MakecertPrivate MakecertPublic LoginSuccessDestinationEnpoint JwtTokenPrivateKey
+echo "\n= .env"
+cat ../customized-env | envsubst | tee .env
+echo "\n= conf-testenv/config.yaml"
+cat ../customized-config.yaml | envsubst | tee conf-testenv/config.yaml
+
+echo "====================================================================="
+echo "===                          INSTALL END                          ==="
+echo "====================================================================="
