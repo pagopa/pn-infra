9c9cf9773854a14bc8308239878d8607e9a3b20f
marco-vit-pagopa <marco.vit@pagopa.it>
2022-10-02 18:47:12 +0200

    PN-2086 aggiunto unzip alla lambda

--- runtime-infra/lambdas/opensearch-delivery/lib/kinesis.js
+++ runtime-infra/lambdas/opensearch-delivery/lib/kinesis.js
@@ -1,15 +1,23 @@
 import { Buffer } from 'node:buffer';
+import { gunzipSync } from 'node:zlib';
+
+function myGunzip( buffer ) {
+  return gunzipSync(buffer) 
+}
+
 
 function decodePayload(b64Str) {
   const payloadBuf = Buffer.from(b64Str, 'base64');
-  return JSON.parse(payloadBuf.toString());
+  const uncompressedBuf = myGunzip( payloadBuf );
+  return JSON.parse(uncompressedBuf.toString('utf8'));
 }
 
 function extractKinesisData(kinesisEvent) {
   return kinesisEvent.Records.map((rec) => {
+    const decodedPayload = decodePayload(rec.kinesis.data);
     return {
       kinesisSeqNumber: rec.kinesis.sequenceNumber,
-      ...decodePayload(rec.kinesis.data)
+      ... decodedPayload
     }
   });
 }
