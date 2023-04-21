a71981f9fe013823612e3d058ab6f0a8f36ed66d
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-04-21 14:05:20 +0200

    PN-4825 edit safestorage-logger lambda

--- runtime-infra/lambdas/safestorage-logger/index.js
+++ runtime-infra/lambdas/safestorage-logger/index.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk')
+const zlib = require('zlib');
+const stream = require('stream');
 
 exports.handler = async (event) => { 
     console.log( JSON.stringify(event, null, 2) );
@@ -17,8 +19,35 @@ exports.handler = async (event) => {
             
             var s3 = new AWS.S3();
             var params = {Bucket: bucketName, Key: fileKey}
-            var data = await readOne( s3, params )  
-            console.log( data )
+            var dataS3 = await readOne( s3, params )
+            console.log("dataS3= ", dataS3)
+
+            // Crea uno stream di lettura dal campo Body
+            const readStream = new stream.PassThrough();
+            readStream.end(Buffer.from(dataS3.Body, 'base64'));
+
+            // Unzip del file
+            const unzippedStream = readStream.pipe(zlib.createGunzip());
+
+            // Legge il contenuto del file unzippato
+            let unzippedBody = '';
+            unzippedStream.on('data', (data) => {
+                unzippedBody += data.toString();
+            });
+
+            // Ritorna una Promise che si risolve dopo che il processo di unzip è completato
+            return new Promise((resolve, reject) => {
+                unzippedStream.on('end', () => {
+                    console.log('Processo di unzip completato.');
+                    console.log('Contenuto del file unzippato:', unzippedBody);
+                    filterAndPrintElement(unzippedBody);
+                    resolve();
+                });
+                unzippedStream.on('error', (error) => {
+                    console.error('Errore durante il processo di unzip:', error);
+                    reject(error);
+                });
+            });
         }
     }
 }
@@ -30,4 +59,61 @@ function readOne(s3, params) {
             if (err) { rec(err) } else { acc(data) }
         });  
     })
+}
+
+function filterAndPrintElement(unzippedBody) {
+    const body = JSON.parse(unzippedBody);
+
+    // Filtra gli oggetti che hanno il valore della chiave "eventName" uguale a "GetObject"
+    const filteredRecords = body.Records.filter(record => record.eventName === "GetObject");
+    filteredRecords.forEach(record => {
+        if(record.requestParameters.hasOwnProperty('x-amzn-trace-id')) {
+            createAuditLog(record, '', 'AUD_DOWNLOAD', 'OK')
+        }
+        else {
+            const userIdentity = record.userIdentity;
+            if(userIdentity.hasOwnProperty('arn') && userIdentity.arn.includes('assumed-role/pn-safe-storage-TaskRole')) {
+                createAuditLog(record, 'Amazon headers are not present!', 'AUD_DOWNLOAD', 'KO')
+            }
+            else {
+                // ignoro l'evento
+            }
+        }
+
+
+    });
+}
+
+function createAuditLog(record, message = '', aud_type, status) {
+    let statusMessage = `INFO - ${message}`;
+    if (status === 'OK') {
+        statusMessage = `OK - SUCCESS - ${message}`;
+    }
+    if (status === 'KO') {
+        statusMessage = `KO - FAILURE - ${message}`;
+    }
+    const auditLogRecord = {
+        name: 'AUDIT_LOG',
+        message: `[${aud_type}] - ${statusMessage}`,
+        aud_type: aud_type,
+        level: status === 'KO' ? 'ERROR' : 'INFO',
+        level_value: status === 'KO' ? 40000 : 20000,
+        logger_name: 'pn-safestorage-logger',
+        eventID: record.eventID,
+        eventName: record.eventName,
+        eventTime: record.eventTime,
+        requestID: record.requestID,
+        bucketFileKey: record.requestParameters ? record.requestParameters.key : null,
+        requestSignedHeaders: record.requestParameters && record.requestParameters.hasOwnProperty('X-Amz-SignedHeaders') ? record.requestParameters['X-Amz-SignedHeaders'] : null,
+        requestSigningAlgorithm: record.requestParameters && record.requestParameters.hasOwnProperty('X-Amz-Algorithm') ? record.requestParameters['X-Amz-Algorithm'] : null,
+        requstDate: record.requestParameters && record.requestParameters.hasOwnProperty('X-Amz-Date') ? record.requestParameters['X-Amz-Date'] : null,
+        trace_id: record.requestParameters && record.requestParameters.hasOwnProperty('x-amzn-trace-id') ? record.requestParameters['x-amzn-trace-id'] : null,
+    };
+    if(status === 'KO') {
+        console.error(JSON.stringify(auditLogRecord));
+    }
+    else {
+        console.log(JSON.stringify(auditLogRecord));
+    }
+
 }
